64f5acfe15586f6231e9d39331f9f2c8436197da
tanvimahal <tanvii.mahal@gmail.com>
2025-03-21 13:40:40 -0600

    Finished

--- WebAnalyzer/web+analyzer.py
+++ WebAnalyzer/web+analyzer.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
 url = "https://en.wikipedia.org/wiki/University_of_Calgary"
 try:
     response = requests.get(url)
@@ -11,30 +13,30 @@ except Exception as e:
 
 # print(soup.prettify())
 
-acount = 0
+#part 3: data analysis
+links = 0
 atags = soup.find_all('a')
 for i in atags:
-    acount += 1
-print(acount)
+    links += 1
+print(links)
 
-pcount = 0
+paragraphs = 0
 ptags = soup.find_all('p')
 for i in ptags:
-    pcount += 1
-print(pcount)
+    paragraphs += 1
+print(paragraphs)
 
 headers=["h1","h2","h3","h4","h5","h6"]
 
-hcount = 0
+headings = 0
 for x in headers:
     htags = soup.find_all(x)
     for i in htags:
-        hcount += 1
-print(hcount)
+        headings += 1
+print(headings)
 
-
+#part 5: Word Frequency Analysis
 words = soup.get_text().split()
-
 wordcounts = {}
 
 for word in words:
@@ -61,3 +63,31 @@ for i in range(5):
 
 for word, count in frequentwords:
     print(f"{word}: {count}")
+
+# part 6: Finding the Longest Paragraph
+
+parawordcounts = {}
+maxwordcount = 0
+longestparagraph = ""
+
+for paragraph in ptags:
+    words = paragraph.get_text().split()
+    
+    wordcount = len(words)
+
+    if wordcount >= 5 and wordcount > maxwordcount:
+        longestparagraph = paragraph.get_text()
+        maxwordcount = wordcount
+
+print(f"Longest Paragraph: {longestparagraph}, Number of words: {maxwordcount}")
+
+
+
+# part 7: Visualizing Results
+
+labels = ['Headings', 'Links', 'Paragraphs']
+values = [headings, links, paragraphs]
+plt.bar(labels, values)
+plt.title('Group 9')
+plt.ylabel('Count')
+plt.show()
