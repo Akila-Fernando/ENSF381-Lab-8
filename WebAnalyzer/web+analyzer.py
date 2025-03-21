adb3b5db1c4ac74abc5bf8b7b39f417407b6aba0
tanvimahal <tanvii.mahal@gmail.com>
2025-03-21 11:34:33 -0600

    part3

--- WebAnalyzer/web+analyzer.py
+++ WebAnalyzer/web+analyzer.py
@@ -10,3 +10,26 @@ except Exception as e:
     print(f"Error fetching content: {e}")
 
 print(soup.prettify())
+
+acount = 0
+atags = soup.find_all('a')
+for i in atags:
+    acount += 1
+print(acount)
+
+pcount = 0
+ptags = soup.find_all('p')
+for i in ptags:
+    pcount += 1
+print(pcount)
+
+headers=["h1","h2","h3","h4","h5","h6"]
+
+hcount = 0
+for x in headers:
+    htags = soup.find_all(x)
+    for i in htags:
+        hcount += 1
+print(hcount)
+
+    
